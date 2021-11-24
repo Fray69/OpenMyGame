af744484da326de742362a8dfc8113e0f845c8bc
Fray69 <77903798+Fray69@users.noreply.github.com>
2021-11-24 14:09:22 +0300

    feat: final solution Task4

---
+++ task4/task4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Строка: ");
+            string s = Console.ReadLine();
+
+            if (IsValid(s)) Console.WriteLine("Правильная последовательность");
+            else Console.WriteLine("Неправильная");
+
+            Console.ReadKey();
+        }
+
+ 
+        static bool IsValid(string input)
+        {
+            Stack<char> buffer = new Stack<char>();
+
+            bool isValid = true;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case '(':
+                    case '{':
+                    case '[':
+                        buffer.Push(input[i]);
+                        break;
+
+                    case ')':
+                        isValid = (buffer.Count > 0 && buffer.Pop() == '(');
+                        break;
+
+                    case '}':
+                        isValid = (buffer.Count > 0 && buffer.Pop() == '{');
+                        break;
+
+                    case ']':
+                        isValid = (buffer.Count > 0 && buffer.Pop() == '[');
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if (!isValid) break; 
+            }
+
+            return isValid && buffer.Count == 0; 
+        }
+    }
+}
