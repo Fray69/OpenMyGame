5da29d74544a1b13ee65c1ddd9d07e7e02af2a8b
Fray69 <77903798+Fray69@users.noreply.github.com>
2021-11-24 14:07:42 +0300

    feat: final solution Task2

---
+++ task2/task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите количество аргументов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            //string[] ams = { "кот", "ток", "кто" };  
+            string[] ams = new string[n];
+            for (int i = 0; i < n ; i++)
+            {
+                Console.Write("Аргумент {0}: ", i+1);
+                ams[i] = Convert.ToString(Console.ReadLine());
+            }
+
+            char[] ch;
+
+            for (int i = 0; i < ams.Length; i++) 
+            {
+                ch = ams[i].ToArray();        
+                Array.Sort(ch);
+                ams[i] = new string(ch);
+
+            }
+            /*for (int i = 0; i < ams.Length; i++)
+                Console.WriteLine(ams[i]);*/
+
+            int check = 1;
+            for (int i = 0; i < ams.Length - 1; i++)
+                if(ams[i] == ams[i+1])
+                    check = 1;
+                else
+                    check = 0;
+
+            if (check == 1)
+                Console.WriteLine("true");
+            else
+                Console.WriteLine("false");
+
+            Console.ReadLine();
+        }
+    }
+}
