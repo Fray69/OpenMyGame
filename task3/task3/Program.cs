5b1bf688725d6a241c4a04f3cbdfe6433c05e356
Fray69 <77903798+Fray69@users.noreply.github.com>
2021-11-24 14:08:32 +0300

    feat: final solution Task3

---
+++ task3/task3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Строка: ");
+            string s = Console.ReadLine();
+            s = s.Replace(" ", "");
+            s = s.Replace(",", "");
+            s = s.Replace(".", "");
+            s = s.Replace("!", "");
+            s = s.Replace("?", "");
+            s = s.ToLower();
+
+            if (IsPalindrome(s)) Console.WriteLine("Полиндром");
+            else Console.WriteLine("Не полиндром");
+
+            Console.ReadKey();
+        }
+
+        static bool IsPalindrome(string s)
+        {
+            char[] b = s.ToCharArray();
+            Array.Reverse(b);
+            return s == new String(b);
+        }
+    }
+}
