d9ff657ea2fbca244cd2430faeb9f8003c472456
Fray69 <77903798+Fray69@users.noreply.github.com>
2021-11-24 14:06:26 +0300

    feat: final solution Task1

---
+++ task1/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите год: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0){
+                Console.WriteLine("Высокосный");}
+            else{
+                Console.WriteLine("Не высокосный");}
+        }
+    }
+}
